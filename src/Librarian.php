a9d599213155317f52461a8eaed3876cc71a39e6
Marian Dobre <marian.dobre@rinf.ro>
2015-03-31 18:48:23 +0300

    Add CI instance as external dependency

--- src/Librarian.php
+++ src/Librarian.php
@@ -2,8 +2,24 @@
 
 class Librarian
 {
+    private $CI;
+
+    public function __construct($CI)
+    {
+        $this->CI = $CI;
+
+        $this->CI->load->library('orm');
+        $this->CI->load->library('form_validation');
+        $this->CI->load->helper('url');
+    }
+
     public function getAge()
     {
         return 44;
+    }
+
+    public function getURL()
+    {
+        return current_url();
     }
 }
